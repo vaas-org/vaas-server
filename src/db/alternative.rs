502b4d69d1208568c705d1bef747c4cc1401cb76
Christian Duvholt <christian@duvholt.net>
2020-07-07 23:09:33 +0200

    Rewrite AlternativesForIssueId to use async message handler as an example

--- src/db/alternative.rs
+++ src/db/alternative.rs
@@ -1,12 +1,11 @@
 use super::{issue::IssueId, DbExecutor};
-use crate::message_handler_with_span;
-use crate::span::SpanHandler;
+use crate::{async_message_handler_with_span, span::AsyncSpanHandler};
 use actix::prelude::*;
-use actix_interop::{with_ctx, FutureInterop};
+use actix_interop::with_ctx;
 use color_eyre::eyre::Report;
 use serde::{Deserialize, Serialize};
 use sqlx::types::Uuid;
-use tracing::{debug, Span};
+use tracing::{debug, instrument};
 
 #[derive(Clone, Hash, PartialEq, Eq, Debug, Deserialize, Serialize, sqlx::Type)]
 #[sqlx(transparent)]
@@ -25,28 +24,24 @@ pub struct InternalAlternative {
     pub issue_id: IssueId,
 }
 
-#[derive(Message, Clone)]
+#[derive(Message, Clone, Debug)]
 #[rtype(result = "Result<Vec<InternalAlternative>, Report>")]
 pub struct AlternativesForIssueId(pub IssueId);
 
-message_handler_with_span! {
-    impl SpanHandler<AlternativesForIssueId> for DbExecutor {
-        type Result = ResponseActFuture<Self, <AlternativesForIssueId as Message>::Result>;
-
-        fn handle(&mut self, msg: AlternativesForIssueId, _ctx: &mut Context<Self>, _span: Span) -> Self::Result {
-            async {
-                let pool = with_ctx(|a: &mut DbExecutor, _| a.pool());
-                let alternative_id = msg.0;
-                let uuid = alternative_id.0;
-                debug!("Retrieving alternative by id {id}", id = uuid);
-                let user = sqlx::query_as!(InternalAlternative,
-                    r#"
-                    SELECT id as "id: _", title, issue_id as "issue_id: _" FROM alternatives WHERE issue_id = $1
-                    "#, uuid
-                ).fetch_all(&pool).await?;
-
-                Ok(user)
-            }.interop_actor_boxed(self)
+async_message_handler_with_span! {
+    impl AsyncSpanHandler<AlternativesForIssueId> for DbExecutor {
+        #[instrument]
+        async fn handle(msg: AlternativesForIssueId) -> Result<Vec<InternalAlternative>, Report> {
+            let pool = with_ctx(|a: &mut DbExecutor, _| a.pool());
+            let alternative_id = msg.0;
+            let uuid = alternative_id.0;
+            debug!("Retrieving alternative by id {id}", id = uuid);
+            let user = sqlx::query_as!(InternalAlternative,
+                                r#"
+                                SELECT id as "id: _", title, issue_id as "issue_id: _" FROM alternatives WHERE issue_id = $1
+                                "#, uuid
+                            ).fetch_all(&pool).await?;
+            Ok(user)
         }
     }
 }
